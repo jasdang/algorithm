1bc9004e951e7fedc5527d0cf6f5325fb37c17ae
Jasmine Dang <ngandth.1992@gmail.com>
2020-04-15 00:14:30 +0800

    Iterate through the matrix to get row id and col id of 0. Yet to replace values for the entire row and col of 0

---
+++ 01-Arrays-and-Strings/08_zero_matrix.rb
@@ -0,0 +1,35 @@
+def zero_matrix(matrix)
+  # TODO: if an element in the MxN matrix is 0,
+  # its entire row and column are set to 0
+  row_ids = []
+  col_ids = []
+  matrix.each_with_index do |row, i|
+    row. each_with_index do |val, j|
+      if val.zero?
+        row_ids << i
+        col_ids << j
+        puts "run"
+      end
+      # puts "#{val.zero?} at #{i} and #{j}"
+    end
+  end
+  p row_ids
+  p col_ids
+  p matrix
+  matrix.map!.with_index do |row, i|
+    if row_ids.include? i
+      row.map! { |val| val = 0 }
+    else
+      col_ids.each { |j| row[j] = 0 }
+    end
+  end
+  matrix
+end
+
+matrix = [[0, 1, 2, 3, 4, 5, 6, 7],
+          [1, 1, 2, 0, 4, 5, 6, 7],
+          [1, 1, 2, 3, 4, 5, 6, 7],
+          [1, 1, 2, 0, 4, 5, 6, 7],
+          [1, 1, 2, 3, 4, 5, 6, 7]]
+
+p zero_matrix(matrix)
